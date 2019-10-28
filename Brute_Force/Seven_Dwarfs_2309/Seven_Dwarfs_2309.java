badebc7213d344db46c16388ede2da984fcd5f9c
DevStevenLee <mr938363@gmail.com>
2019-10-28 19:26:14 +0900

    Write comments

--- Brute_Force/Seven_Dwarfs_2309/Seven_Dwarfs_2309.java
+++ Brute_Force/Seven_Dwarfs_2309/Seven_Dwarfs_2309.java
@@ -5,11 +5,19 @@ class Seven_Dwarfs_2309{
 	private static List<Integer> res = new ArrayList<>();
 	
 	public static void brute_force(int[] dwarfs, List<Integer> candidate, int now, int sum){
+		/* If now index is out of the range, it should return */
 		if(now >= 9) return;
+
+		/* If it detects to what we're looking for, clone the candidate to the result */
 		if(candidate.size() == 6 && sum + dwarfs[now] == 100 && res.isEmpty()){
 			candidate.add(dwarfs[now]);
 			res = new ArrayList<>(candidate);
 		}
+
+		/* There are two choices for each dwarf that pick one or not.
+		 * Here, first recurisve function supposed to add a dwarf.
+		 * Next, second recurisve function supposed to pass a dwarf.
+		 */
 		else{
 			candidate.add(dwarfs[now]);
 			brute_force(dwarfs, candidate, now + 1, sum + dwarfs[now]);
