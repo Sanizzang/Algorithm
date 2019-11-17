bccf790d73c8409789cd08df727e78e596b5b6a8
DevStevenLee <mr938363@gmail.com>
2019-11-17 16:26:59 +0900

    Comment on the good subsequence code

--- Backtracking/GoodSubsequence_2661/GoodSubsequence_2661.java
+++ Backtracking/GoodSubsequence_2661/GoodSubsequence_2661.java
@@ -5,9 +5,13 @@ class GoodSubsequence_2661{
 	private static int N;
 	public static boolean isSubsequencesDuplicated(String candidate){
 		int len = candidate.length();
-	
+
+		/* i is the start of subsequence.
+		 * j is the length of subsequence.
+		 * */
 		for(int i = 0 ; i < len ; i++){
 			for(int j = 1 ; i + 2*j - 1 < len ; j++){
+				/* If it's duplicated, return true. */
 				if(candidate.substring(i, i+j).equals(candidate.substring(i+j, i+2*j)))
 					return true;
 			}
@@ -22,6 +26,7 @@ class GoodSubsequence_2661{
 		
 		String res = "";
 		for(int i = 1 ; i <= 3 ; i++){
+			/* If it doesn't find the result yet or its subsequences aren't duplicated, go next*/
 			if(res.length() != N && !isSubsequencesDuplicated(candidate + i)){
 				res = makeTheSequence(candidate + i);
 			}
