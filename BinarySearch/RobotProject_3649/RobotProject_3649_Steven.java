4225d0feb5f5aa98f31f8952019267452c5a3915
DevStevenLee <mr938363@gmail.com>
2019-12-10 19:20:40 +0900

    Resolving Robot Project 3649 in Binary Search

--- BinarySearch/RobotProject_3649/RobotProject_3649_Steven.java
+++ BinarySearch/RobotProject_3649/RobotProject_3649_Steven.java
@@ -18,22 +18,29 @@ class RobotProject_3649_Steven{
 	}
 
 	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		while(sc.hasNextInt()){
-			H = sc.nextInt() * MAX;
-			N = sc.nextInt();
+		String input;
+		
+		while((input = br.readLine()) != null){
+			H = Integer.parseInt(input) * MAX;
+			N = Integer.parseInt(br.readLine());
 
 			int[] legos = new int[N];
 
 			for(int i = 0 ; i < N ; i++){
-				legos[i] = sc.nextInt();
+				legos[i] = Integer.parseInt(br.readLine());
 			}
 
 			Arrays.sort(legos);
 
 			int l1 = 0, l2 = 0;
 			for(int i = 0 ; i < N - 1 ; i++){
+				/* If H - legos[i] > 0, H - legos[i] could exist in the legos 
+				 * Find (H - l(k)) in range of (k + 1) to N - 1
+				 * 	because, definition of l is l2 >= l1
+				 * */
 				if(H - legos[i] > 0 && binarySearch(legos, i + 1, N - 1, H - legos[i])){
 					l1 = legos[i];
 					l2 = H - legos[i];
@@ -41,12 +48,15 @@ class RobotProject_3649_Steven{
 				}
 			}
 
-			if(l1 == 0 && l2 == 0) System.out.println("danger");
-			else System.out.println("yes " + l1 + " " + l2);
+			if(l1 == 0 && l2 == 0) bw.write("danger" + "\n");
+			else bw.write("yes " + l1 + " " + l2 + "\n");
 
 		}
 
-		sc.close();
+		bw.flush();
+
+		br.close();
+		bw.close();
 	}
 
 }
